69c394ebbf3b473ee2ce4c6dac8462b92a5a308c
dmoon <dmoon.t@gmail.com>
2017-09-05 03:27:18 +0800

    Add infinite scroll feature

--- sample/flatList/index.js
+++ sample/flatList/index.js
@@ -7,43 +7,57 @@ import {
   Alert
 } from 'react-native';
 
+
+const DATA = [
+  { key: 'AAAAAA' },
+  { key: 'BBBBBB' },
+  { key: 'CCCCCC' },
+  { key: 'DDDDDD' },
+  { key: 'EEEEEE' },
+  { key: 'FFFFFF' },
+  { key: 'GGGGGG' },
+];
 export default class FlatListSample extends Component {
+  state = {
+    count: 1,
+    list: DATA
+  }
+
+  onEndReached = () => {
+    Alert.alert('阿', '到底摟');
+    this.setState((prevState, props) => ({
+      list: [
+        ...prevState.list,
+        ...DATA.map((item) => ({ key: prevState.count + item.key }))
+      ],
+      count: prevState.count + 1
+    }));
+  }
+
   render() {
     return (
       <FlatList
-        data={[
-          {key: 'AAAAAA'},
-          {key: 'BBBBBB'},
-          {key: 'CCCCCC'},
-          {key: 'DDDDDD'},
-          {key: 'EEEEEE'},
-          {key: 'FFFFFF'},
-          {key: 'GGGGGG'},
-          {key: 'HHHHHH'},
-          {key: 'IIIIII'},
-          {key: 'JJJJJJ'},
-          {key: 'KKKKKK'},
-          {key: 'LLLLLL'},
-        ]}
-        renderItem={({item}) => <View style={{ height: 150  }}><Text>{item.key}</Text></View>}
-        onEndReached={() => { Alert.alert('阿', '到底摟') }}
+        data={this.state.list}
+        renderItem={({ item }) => <View style={{ height: 200, justifyContent: 'center', alignItems: 'center' }}><Text>{item.key}</Text></View>}
+        onEndReachedThreshold={0.1}
+        onEndReached={this.onEndReached}
         ListHeaderComponent={
           <View style={[styles.container, { backgroundColor: 'blue' }]}>
-            <Text style={{ color: '#fff'  }}>Header</Text>
+            <Text style={{ color: '#fff' }}>Header</Text>
           </View>
         }
         ListFooterComponent={
           <View style={[styles.container, { backgroundColor: 'green' }]}>
-            <Text style={{ color: '#fff'  }}>Footer</Text>
+            <Text style={{ color: '#fff' }}>Footer</Text>
           </View>
         }
         ListEmptyComponent={
           <View style={[styles.container, { backgroundColor: 'red' }]}>
-            <Text style={{ color: '#fff'  }}>Empty</Text>
+            <Text style={{ color: '#fff' }}>Empty</Text>
           </View>
         }
         ItemSeparatorComponent={
-          ({highlighted}) => <View style={{ height: 3, backgroundColor: 'pink'  }} />
+          ({ highlighted }) => <View style={{ height: 3, backgroundColor: 'pink' }} />
         }
       />
     );
